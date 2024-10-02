e38dcfe940f68d78a2450118a859b41d94b4c51c
Vikram Narra <vchandra.narra@gmail.com>
2024-10-02 13:42:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def get_celebrity_info():
     })
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8000)), debug=True)
